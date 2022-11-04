60beb05a3f033b6fe8b8a48b14453bcf4723c3d1
DavidPerezo <davidwolfsangel@gmail.com>
2022-11-04 00:13:48 -0400

    v1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
+import { Usuario } from './models/usuario';
+import {FormsModule} from '@angular/forms'
+import { bootstrapApplication, BrowserModule } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Examen2';
+  title(title: any) {
+    throw new Error('Method not implemented.');
+  }
+  UsuarioArray: Usuario[] = [
+   {codigo: 1, nombre: "Silla", descripcion: "Hecho en Bolivia", dim: "A", color: "Rojo", materialesID: "1"},
+   {codigo: 2, nombre: "Mesa", descripcion: "Hecho en Bolivia", dim: "A", color: "Azul", materialesID: "2"},
+   {codigo: 3, nombre: "Ropero", descripcion: "Hecho en Bolivia", dim: "A", color: "Amarillo", materialesID: "3"}
+
+
+  ];
+  selectedUsuario: Usuario = new Usuario();
 }
---
+++ src/app/models/usuario.ts
@@ -0,0 +1,9 @@
+export class Usuario {
+  codigo!: number;
+  nombre : string | undefined
+  descripcion : string | undefined;
+  dim : string | undefined
+  color : string | undefined
+  materialesID : string | undefined
+}
+
